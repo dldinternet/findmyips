f7b3475b7b2c6ad11093954f2964f2ca8b13a3ef
Christo De Lange <github@dldinternet.com>
2013-08-12 20:58:54 -0400

    First working version

---
+++ bin/whatismyip.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#source /Users/christo/.rvm/scripts/rvm
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+#eval "$(rbenv init -)"
+
+#rvm list
+rvm use 1.9.3@whatismyip >/dev/null
+ruby `dirname $0`/whatismyip.rb
